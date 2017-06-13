5da9102169d14a1d1934b4cfa21dfbf2011f71b2
Yang,xue-peng <xue-peng.yang@hpe.com>
2017-06-13 16:13:24 +0800

    First commit

---
+++ src/main/java/com/it/controller/TestSSH.java
@@ -0,0 +1,22 @@
+package com.it.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class TestSSH {
+	private static String SUCCESS = "success";
+	@RequestMapping("/springMVC")
+	public String testSprintMVC(){
+		System.out.println("test spring MVC");
+		return SUCCESS;
+	}
+	public TestSSH(){
+		System.out.println("Init testSSH...");
+	}
+	public void initGit(){
+		
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href="test/springMVC">Test Spring MVC</a>
+</body>
+</html>
